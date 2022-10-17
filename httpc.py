2517dd8ced67dbdf0ab1d51f1f68fa6dbbeda36f
Julie Manirath <juliemanirath@gmail.com>
2022-10-17 16:03:08 -0400

    additional parse option added for get and post command

--- httpc.py
+++ httpc.py
@@ -1,14 +1,16 @@
 import argparse
+import socket
+from sys import exit
+from urllib.parse import urlparse
 
 # httpc (get|post) [-v] (-h "k:v")* [-d inline-data] [-f file] URL
 parser = argparse.ArgumentParser(add_help=False)
-
 subparser = parser.add_subparsers()
 parser_list = subparser.add_parser('help')
 parser_list.add_argument('help_type', nargs='?', default='help', choices=['help','get','post'])
-
 parser.add_argument('command', nargs='?' , type=str, choices=['get', 'post'])
 parser.add_argument('-v', dest='verbose', action='store_true')
+parser.add_argument('-h', dest='header', help='Associates headers to HTTP request with the format \'key:value\'', action='append')
 parser.add_argument('-d', dest='data', type=str)
 parser.add_argument('-f', dest='file', type=str)
 parser.add_argument('-url', dest='url', type=str)
@@ -31,3 +33,40 @@ elif args.help_type == 'post':
         'with the format "key:value".\n\t-d string\tAssociates an inline data to the body HTTP POST request.' \
         '\n\t-f file\t\tAssociates the content of a file to the body HTTP POST request.\n\nEither [-d] or [-f] ' \
         'can be used but not both.')
+
+def get_request(url, v, h):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((url.netloc, 80))
+
+def post_request(url, v, h, d, f):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((url.netloc, 80))
+
+# GET option should not be used with the options -d or -f.
+if args.command == 'get':
+    if args.data or args.file:
+        print("Error: -d or -f cannot be used in a GET command.")
+        exit()
+    elif args.url:
+        response_url = args.url.replace("'", "")
+        parsed_response_url = urlparse(response_url)
+        get_request(parsed_response_url, args.verbose, args.header)
+    else:
+        print('Error: no URL has been specified. URL is required after "get"')
+        exit()
+
+# POST option should have either -d or -f but not both. 
+elif args.command == 'post':
+    if args.data and args.file:
+       print("Error: -d and -f can't be used in the same POST command.")
+       exit()
+    elif args.data == None and args.file == None:
+        print("Atleast one of -d or -f should be in a POST command.")
+        exit()
+    if args.url:
+        response_url = args.url.replace("'", "")
+        parsed_response_url = urlparse(response_url)
+        post_request(parsed_response_url, args.verbose, args.header, args.data, args.file)
+    else:
+        print('Error: no URL has been specified. URL is required after "post"')
+        exit()
